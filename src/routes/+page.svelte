de87d42c3382f5e8899bab77e3bb75f9c0a3c99f
Alex Warda <alexsio03@gmail.com>
2024-01-18 22:14:43 +0100

    Fixed flowbite version bug + added to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,46 @@
 <script>
-    import { Alert } from "flowbite-svelte"
 </script>
 
-<div class="p-6">
-    <h1>Welcome to SvelteKit</h1>
-    <Alert>
-        <span class="font-medium">Info alert!</span>
-        Hello World
-    </Alert>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-screen dark:bg-zinc-800 dark:text-zinc-400 p-6">
+    <div class="flex mx-auto p-4">
+        <h1 class="text-zinc-200 text-6xl">LearnCS</h1>
+    </div>
+    <hr class="h-px my-6 bg-zinc-200 border-0 dark:bg-zinc-700">
+    <div>
+        <a class="text-2xl text-zinc-300" href="/data-structures">Data Structures</a>
+        <p>Explore different data structures and learn how to implement them in various programming languages.</p>
+    </div>
+    <hr class="h-px my-6 bg-zinc-200 border-0 dark:bg-zinc-700">
+    <div>
+        <p class="text-2xl text-zinc-300">Algorithms</p>
+        <p>Discover various algorithms and their implementations to solve common programming problems.</p>
+        <div class="flex text-zinc-300 justify-evenly pt-8">
+            <a href="/algos/sorting">Sorting Algorithms</a>
+            <a href="/algos/searching">Searching Algorithms</a>
+            <a href="/algos/path-finding">Path Finding Algorithms</a>
+            <p>|</p>
+            <a href="/algos/big-o">Big O Notation</a>
+        </div>
+    </div>
+    <hr class="h-px my-6 bg-zinc-200 border-0 dark:bg-zinc-700">
+    <div>
+        <a class="text-2xl text-zinc-300" href="/languages">Languages</a>
+        <p>Learn about different programming languages, their features, and use cases.</p>
+    </div>
+    <hr class="h-px my-6 bg-zinc-200 border-0 dark:bg-zinc-700">
+    <div class="flex justify-between">
+        <div class="flex flex-col">
+            <a class="text-2xl text-zinc-300" href="/faq">FAQ</a>
+            <p>Find answers to frequently asked questions about programming and computer science.</p>
+        </div>
+        <div class="flex flex-col">
+            <a class="text-2xl text-zinc-300" href="/journey">My Journey</a>
+            <p>Follow my personal journey in learning computer science and programming.</p>
+        </div>
+        <div class="flex flex-col">
+            <a class="text-2xl text-zinc-300" href="/about">About Me</a>
+            <p>Learn more about me and my background in computer science.</p>
+        </div>
+    </div>
+    <hr class="h-px my-6 bg-zinc-200 border-0 dark:bg-zinc-700">
 </div>
